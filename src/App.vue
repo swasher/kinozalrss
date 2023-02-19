b5016bfdb065164b3ce432230baa609aa536f5d4
Alexey <mr.swasher@gmail.com>
2023-02-19 13:27:28 +0200

    before switch to back4app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
-    <h4>Get data from firestore</h4>
+    <header>
+<!--
+        <a href="#/">Home</a> |
+        <a href="#/about">Parser</a>
+        <component :is="currentView" />
+-->
 
-    <button type="button" class="btn btn-primary" @click="getMovies">Get Data</button>
 
-    <table>
-        <tr v-for="(movie, key) in movies">
-            <td>{{ movie.title }}</td>
-            <td>{{ movie.year }}</td>
-        </tr>
-    </table>
+        <Toolbar>
+            <template #start>
+                <a href="#/">Home  </a> |
+                <a href="#/about">  Parser</a>
+
+<!--                <Button hre label="New" icon="pi pi-plus" class="p-button-link" />-->
+<!--                <Button label="Fetch" icon="pi pi-data" class="p-button-link" />-->
+<!--                <i class="pi pi-bars p-toolbar-separator mr-2" />-->
+<!--                <SplitButton label="Save" icon="pi pi-check" :model="items" class="p-button-warning"></SplitButton>-->
+            </template>
+
+            <template #end>
+                <Button icon="pi pi-search" class="p-button-sm mr-2" />
+            </template>
+        </Toolbar>
+
+    </header>
+
+
+    <Button>frfre</Button>
+    <Button>wefervfe</Button>
+
+    <article>
+        <h4>Get data from firestore</h4>
+
+        <button type="button" class="btn btn-primary" @click="getMovies">Get Data</button>
+
+        <table>
+            <tr v-for="(movie, key) in movies">
+                <td>{{ movie.title }}</td>
+                <td>{{ movie.year }}</td>
+            </tr>
+        </table>
+    </article>
+
+    <article>
+        <h4>Create an Account</h4>
+        <p><input type="text" placeholder="Email" v-model="email" /></p>
+        <p><input type="password" placeholder="Password" v-model="password" /></p>
+        <p><button @click="register">Submit</button></p>
+    </article>
 
 </template>
 
@@ -34,4 +73,48 @@ async function getMovies() {
     movies.value = moviesSnapshot.docs.map(doc => doc.data());
 }
 
+
+const email = ref('')
+const password = ref('')
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+const auth = getAuth();
+const register = () => {
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log('Successfully registered!');
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error.message, ' ### Error code:', error.code, )
+        });
+}
+
 </script>
+
+<style scoped>
+.p-button {
+    margin-right: 0.5rem;
+}
+
+body #app header {
+    margin: 0;
+    padding: 0;
+}
+
+
+.template .p-button i {
+    line-height: 2.25rem;
+}
+
+Toolbar {
+    display: flex;
+    width: 100vw;
+}
+
+.wrapper{
+    padding-left: 30px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import { createApp } from 'vue'
 // import { auth } from './firebase'
 import App from './App.vue'
 
-// import './assets/main.css'
+import PrimeVue from 'primevue/config';
+import Panel from 'primevue/panel';
+// import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.use(PrimeVue);
+// app.component('Button', Button);
+app.component('Panel', Panel);
+app.mount('#app');
+
